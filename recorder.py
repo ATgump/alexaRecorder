98f79b76ff3f11d083a6707733ca1e19e03eb048
Avery Gump <averygmp@gmail.com>
2022-04-19 02:12:22 -0400

    finished with the recorder/scraper (almost)

--- recorder.py
+++ recorder.py
@@ -1,19 +1,26 @@
 from playsound import playsound
 import os
 import time
-from scrape import scraper
-tess_path = 'C:/Users/avery/OneDrive/Desktop/TessAudio/TESS_Toronto_emotional_speech_set_data/OAF_angry/'
-
-tess_folder = os.listdir(tess_path)
+from scrape import scrapeAudio
+audio_data_path = "C:\\Users\\avery\\Documents\\alexaExperimentAudioFilesTEST"
+start_time = time.time()
+experiment_audio_folder = os.listdir(audio_data_path)
 i = 0
-#s = scraper
-alexa_open_prompt = "C:/Users/avery/OneDrive/Desktop/alexaOpen.mp3"
-for filename in tess_folder:
-    playsound(alexa_open_prompt)
-    p = tess_path+filename
-    playsound(p)
-    #s.scrapeAudio("PATH",filename)
-    time.sleep(5)
-    i = i+1
-    if i == 20:
-        break
+alexa_open_prompt = "C:/Users/avery/OneDrive/Desktop/echoOpen.mp3"
+for folder in experiment_audio_folder:
+    """     if folder == "audio_speech_actors_01-24":
+        need_name = True
+    else:
+        need_name = False """
+    p = audio_data_path+"\\"+folder
+    audio_folders = os.listdir(p)
+    for data_folder in audio_folders:
+        for audio_file in os.listdir(p+"\\"+data_folder):
+            playsound(alexa_open_prompt)
+            playsound(p+"\\"+data_folder+"\\"+audio_file)
+            time.sleep(15)
+            scrapeAudio(data_folder+"_"+audio_file+"_ALEXA")
+end_time = time.time()
+time_taken = str(end_time-start_time)
+with open ("C:\\Users\\avery\\OneDrive\\Desktop\\TIME_TAKEN_TO_RUN_24.txt",'w') as f:
+    f.write(time_taken)
--- scrape.py
+++ scrape.py
@@ -1,18 +1,103 @@
-import selenium
-import requests
-from bs4 import BeautifulSoup
-    # function to extract html document from given url
-def getHTMLdocument(url):
-    # request for HTML document of given url
-    response = requests.get(url)  
-    # response will be provided in JSON format
-    return response.text
+from ipaddress import collapse_addresses
+import random
+from selenium import webdriver
+from selenium.webdriver.common import proxy
+import pyautogui
+from chromedriver_py import binary_path # this will get you the path variable
+from selenium.webdriver.chrome.service import Service
+import json
+import random
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import time
 
 
-class scraper():
-    def scrapeAudio(direc, name):
-        history_url = "https://www.amazon.com/alexa-privacy/apd/rvh?"
-        html_resp = getHTMLdocument(history_url)
-        soup = BeautifulSoup(html_resp, 'html.parser')
-        print(soup.prettify)
-        return 0
+def randSleepTime(x,y):
+    n = random.randint(x,y)
+    time.sleep(n)
+    return 0
+def process_browser_logs_for_network_events(logs):
+    for entry in logs:
+        log = json.loads(entry["message"])["message"]
+        if (
+            "Network.response" in log["method"]
+            or "Network.request" in log["method"]
+            or "Network.webSocket" in log["method"]
+        ):
+            yield log
+def delAudio(drive):
+    boxes = drive.find_elements_by_class_name("apd-content-box")
+    for box in boxes:
+            expandButton = box.find_element_by_class_name("apd-expand-toggle-button.button-clear")
+            expandButton.click()
+            randSleepTime(2,12)
+            delBox = box.find_element_by_class_name("record-delete-banner.clickable")
+            delButton = delBox.find_element_by_class_name("button-clear.apd-icon-button-round.record-delete-button")
+            delButton.click()
+            randSleepTime(2,12)
+
+def scrapeAudio(name):
+    pyautogui.keyDown("ctrl")
+    pyautogui.keyDown("shift")
+    pyautogui.press("m")
+    pyautogui.keyUp("ctrl")
+    pyautogui.keyUp("shift")
+    service_object = Service(binary_path)
+    option = webdriver.ChromeOptions()
+    capabilities = DesiredCapabilities.CHROME
+    capabilities["goog:loggingPrefs"] = {"performance": "ALL"}
+    option.add_argument("user-data-dir=C:\\Users\\avery\\AppData\\Local\\Google\\Chrome\\User Data")
+    option.add_argument("profile-directory=Profile 1")
+    option.add_experimental_option("detach",True)
+    driver = webdriver.Chrome(service=service_object,options=option,desired_capabilities=capabilities)
+    driver.get("https://www.amazon.com/alexa-privacy/apd/rvh?")
+    randSleepTime(2,12)
+    boxes = driver.find_elements_by_class_name("apd-content-box")
+    for box in boxes:
+        try:
+            transcript = box.find_element_by_class_name("record-summary-preview.customer-transcript").text
+            if transcript.startswith('"say') or transcript.startswith('"kids') or transcript.startswith('"dogs'):
+                expandButton = box.find_element_by_class_name("apd-expand-toggle-button.button-clear")
+                expandButton.click()
+                randSleepTime(2,12)
+                recBox = box.find_element_by_class_name("record-item")
+                recButton = recBox.find_element_by_class_name("apd-icon-button-round.play-audio-button.button-clear.fa.fa-play-circle")
+                recButton.click()
+                randSleepTime(2,12)
+                perf = driver.get_log("performance")
+                events = process_browser_logs_for_network_events(perf)
+                audio_url = "didnt get updated"
+                for event in events:
+                    try:
+                        tt = event["params"]["request"]["url"]
+                        if tt.startswith("https://www.amazon.com/alexa-privacy/apd/rvh/audio?uid"):
+                            audio_url = tt
+                    except:
+                        continue
+                collapse_button = box.find_element_by_class_name("apd-expand-toggle-button.button-clear.fa.fa-chevron-up")
+                collapse_button.click()
+                randSleepTime(2,12)
+        except:
+            continue
+            
+    driver.execute_script("window.open('');")
+    driver.switch_to.window(driver.window_handles[1])
+    driver.get(audio_url)
+    pyautogui.keyDown("ctrl")
+    pyautogui.press("s")
+    pyautogui.keyUp("ctrl")
+    time.sleep(2)
+    pyautogui.write(name,interval=.25)
+    time.sleep(2)
+    pyautogui.press("enter")
+    time.sleep(1)
+    driver.switch_to.window(driver.window_handles[0])
+    time.sleep(4)
+    delAudio(driver)
+    driver.close()
+    driver.quit()
+    pyautogui.keyDown("ctrl")
+    pyautogui.keyDown("shift")
+    pyautogui.press("m")
+    pyautogui.keyUp("ctrl")
+    pyautogui.keyUp("shift")
+    return 0
