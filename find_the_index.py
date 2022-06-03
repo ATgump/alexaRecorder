7e847ced156daa4c25f4c9918329e81447c54f09
Avery Gump <averygmp@gmail.com>
2022-06-03 19:28:51 -0400

    save

--- find_the_index.py
+++ find_the_index.py
@@ -1,6 +1,18 @@
 import playsound
 import os
 for i in (range(1,10)):
-    os.system(r'C:\Users\avery\Downloads\nircmd\nircmd.exe setsysvolume 655 "Speakers" '+str(i))
+    os.system(r'C:\Users\avery\Downloads\nircmd\nircmd.exe setsysvolume 64226 "Headphones" '+str(i))
+    
+
+for i in (range(1,10)):
+    os.system(r'C:\Users\avery\Downloads\nircmd\nircmd.exe setsysvolume 655 "Headphones" '+str(i))
     playsound.playsound(r"C:\Users\avery\OneDrive\Desktop\003-001-001-001-003-001-026.wav")
     print(i)
+
+# for i in (range(1,10)):
+#     os.system('C:\\Users\\avery\\Downloads\\nircmd\\nircmd.exe setsysvolume 0 "Headphones" '+str(i))
+
+# for i in (range(1,10)):
+#     os.system('C:\\Users\\avery\\Downloads\\nircmd\\nircmd.exe setsysvolume 64226 "Headphones" '+str(i))
+#     playsound.playsound(r"C:\Users\avery\OneDrive\Desktop\003-001-001-001-003-001-026.wav")
+#     print(i)
